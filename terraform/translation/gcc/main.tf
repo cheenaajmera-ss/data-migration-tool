edc745a22c6cd67789737c89d50c478b7bb0f10e
cheenaajmera-ss <105350327+cheenaajmera-ss@users.noreply.github.com>
2025-11-27 15:06:15 +0530

    Update main.tf

--- terraform/translation/gcc/main.tf
+++ terraform/translation/gcc/main.tf
@@ -351,7 +351,6 @@ resource "null_resource" "upload_common_utils" {
 
 /* Composer 2: Set up firewall rule to allow Composer GKE cluster pods (KubernetesPodOperator) to reach the rest of the VPC network. */
 data "google_container_cluster" "composer_gke_cluster" {
-  count    = (startswith(var.image_version, "composer-2") || startswith(var.image_version, "composer-3")) ? 1 : 0
   count      = startswith(var.image_version, "composer-2") ? 1 : 0
   depends_on = [google_composer_environment.composer_env]
   name     = length(google_composer_environment.composer_env.config[0].gke_cluster) > 0 ? split("/", google_composer_environment.composer_env.config[0].gke_cluster)[5] : ""
@@ -359,7 +358,6 @@ data "google_container_cluster" "composer_gke_cluster" {
 }
 
 resource "google_compute_firewall" "dmt-pod-operator" {
-  count       = (startswith(var.image_version, "composer-2") || startswith(var.image_version, "composer-3")) ? 1 : 0
   count = (startswith(var.image_version, "composer-2") || startswith(var.image_version, "composer-3")) ? 1 : 0
   name        = "dmt-allow-composer-gke-pods"
   network     = google_composer_environment.composer_env.config.0.node_config.0.network
@@ -367,10 +365,9 @@ resource "google_compute_firewall" "dmt-pod-operator" {
 
   priority      = 1001
   direction     = "INGRESS"
-  source_ranges = [data.google_container_cluster.composer_gke_cluster[0].ip_allocation_policy.0.cluster_ipv4_cidr_block]
   source_ranges = startswith(var.image_version, "composer-2") ? [
     data.google_container_cluster.composer_gke_cluster[0].ip_allocation_policy.0.cluster_ipv4_cidr_block
-    ] : [google_composer_environment.composer_env.config[0].node_config[0].ip_allocation_policy[0].cluster_ipv4_cidr_block]
+  ] : [google_composer_environment.composer_env.config[0].node_config[0].ip_allocation_policy[0].cluster_ipv4_cidr_block]
   allow {
     protocol = "all"
   }
